a00a30cabdf466f28563262790a8c7231d24adcf
ahlaiptek <iptekahla@gmail.com>
2026-03-11 11:11:27 +0700

    feat: Integrate Spatie Laravel Permission package for role and permission management

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        DB::beginTransaction();
 
-        User::factory()->create([
+        User::factory(10)->create();
+        
+        Role::create(['name' => 'admin']);
+        Role::create(['name'=> 'member']);
+        $admin = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $admin->assignRole('admin');
+
+        DB::commit();
     }
 }
