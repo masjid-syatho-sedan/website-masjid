5d7636b9cbc255ae3194c36168487ef20a957501
ahlaiptek <iptekahla@gmail.com>
2026-06-20 19:25:43 +0700

    update map

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -294,7 +294,7 @@
 
             <!-- Map -->
             <div class="mt-12 rounded-xl overflow-hidden border-2 border-amber-200 h-96 bg-gray-200">
-                <iframe class="w-full h-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.351906434!2d111.5741508!3d-6.7677443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e77174f0e88ac87%3A0x3bf1a9e8d2fc8601!2sMASJID%20SYATHO%20SEDAN!5e0!3m2!1sen!2sid!4v1740878826857" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe class="w-full h-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.351906434!2d111.5741508!3d-6.7677443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e77110067f85385%3A0xab7d7fde6da441e5!2sMASJID%20SYATHO%20SEDAN!5e0!3m2!1sen!2sid!4v1740878826857" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </section>
